63f058d228bffadd47ef31e7205aeb2604b72764
Diogo Gomes <dgomes@av.it.pt>
2016-10-13 01:31:00 +0100

    Major refactor

---
+++ start.py
@@ -0,0 +1,13 @@
+from server import *
+from human import HumanSnake
+from agent1 import ComputerSnake
+
+#start the game
+if __name__ == "__main__":
+    snake=SnakeGame(hor=30, ver=20, fps=20)
+    snake.SetObstacles(15) #level of obstacles
+    snake.setplayers([  
+        HumanSnake([(12,14)]),
+        ComputerSnake(),
+    ])
+    snake.start()
