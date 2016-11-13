5986253842e54435ed6c2242372a89f77b9aca75
Diogo Gomes <dgomes@av.it.pt>
2016-11-13 23:07:07 +0000

    --disable-video

--- start.py
+++ start.py
@@ -5,11 +5,12 @@ import sys, getopt
 
 #start the game
 def main(argv):
-    inputfile = None 
+    inputfile = None
+    visual = True
     try:
-        opts, args = getopt.getopt(argv,"hm:",["mapfile="])
+        opts, args = getopt.getopt(argv,"hm:",["map=","disable-video"])
     except getopt.GetoptError:
-        print('start.py -m <mapfile>')
+        print('start.py [-m/--map <mapfile> --disable-video]')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -17,8 +18,10 @@ def main(argv):
             sys.exit()
         elif opt in ("-m", "--map"):
             inputfile = arg
-   
-    snake=SnakeGame(hor=60, ver=40, fps=20)
+        elif opt in ("--disable-video"):
+            visual = False 
+
+    snake=SnakeGame(hor=60, ver=40, fps=20, visual=visual)
     snake.setObstacles(15,inputfile) #level of obstacles
     snake.setPlayers([  
         Agent1([snake.playerPos()]),
