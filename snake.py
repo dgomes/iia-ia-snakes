e76fd5443a2a6dfa5fe4f27ec8e3966f999c3a33
Diogo Gomes <dgomes@av.it.pt>
2016-11-14 02:18:59 +0000

    method instead of property update

--- snake.py
+++ snake.py
@@ -9,6 +9,8 @@ class Snake:
         self.IsDead=False
         self.points = 0
         logging.basicConfig(format=':%(levelname)s:%(message)s', level=logging.DEBUG)
+    def updateBody(self, body):
+        self.body = body
     def update(self,points=None, mapsize=None, count=None, agent_time=None):
         pass #send players stats about the game 
     def updateDirection(self,game):
