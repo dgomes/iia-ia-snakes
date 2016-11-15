2ae9595f7ba234e369cf1b56b2b9bad1b5cc2411
Diogo Gomes <dgomes@av.it.pt>
2016-11-15 23:54:14 +0000

    major refactor to support agents as arguments

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 from game import *
 from human import HumanSnake
 from agent1 import Agent1
-from netagent import NetworkAgent
 from maze import Maze
 import importlib
 import asyncio
@@ -10,8 +9,6 @@ import json
 import logging
 import sys, getopt
 
-StudentAgent = Agent1
-StudentAgent_name = "Alice"
 
 #start the game
 def main(argv):
@@ -19,41 +16,61 @@ def main(argv):
     visual = True
     network = False
     url = 'ws://localhost:8765' 
+    url = None
+    StudentAgent = Agent1
+    studentAgent_name = "Agent1"
+    student_url = None
     OponentAgent = Agent1
-    OponentAgent_name = "Bob"
+    sponentAgent_name = "Agent1"
+    oponent_url = None
     try:
-        opts, args = getopt.getopt(argv,"hm:u:a:",["map=","disable-video","url=","agent="])
-    except getopt.GetoptError:
-        print('start.py [-m/--map <mapfile> --disable-video -u/--url websocket_url -a/--agent AgentName]')
+        opts, args = getopt.getopt(argv,"hm:s:o:p",["help","map=","disable-video","student-agent=","oponent-agent=","proxy"])
+    except getopt.GetoptError as e:
+        print(e)
+        print('start.py [-h/--help -m/--map <mapfile> --disable-video -p/--proxy -s/--student-agent AgentName,Name[,websocket] -o/--oponent-name AgentName,Name[,websocket]]')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('start.py -m <mapfile>')
+            print('start.py [-h/--help -m/--map <mapfile> --disable-video -p/--proxy -s/--student-agent AgentName,Name[,websocket] -o/--oponent-name AgentName,Name[,websocket]]')
             sys.exit()
         elif opt in ("-m", "--map"):
             inputfile = arg
         elif opt in ("--disable-video"):
             visual = False 
-        elif opt in ("-u", "--url"):
+        elif opt in ("-p", "--proxy"):
             network = True
-            OponentAgent = NetworkAgent
-            url = arg            
-        elif opt in ("-a", "--agent"):
-            classmodule = importlib.import_module(arg.lower())
-            classInst = getattr(classmodule, arg)
-            StudentAgent = classInst 
+        elif opt in ("-s", "--student-agent"):
+            a = arg.split(',')
+            classmodule = importlib.import_module(a[0].lower())
+            classInst = getattr(classmodule, a[0])
+            StudentAgent = classInst
+            studentAgent_name = a[1]
+            if len(a) > 2:
+                student_url = a[2]
+        elif opt in ("-o", "--oponent-agent"):
+            a = arg.split(',')
+            classmodule = importlib.import_module(a[0].lower())
+            classInst = getattr(classmodule, a[0])
+            OponentAgent = classInst 
+            oponentAgent_name = a[1]
+            if len(a) > 2:
+                oponent_url = a[2]
 
     if network:
-        asyncio.get_event_loop().run_until_complete(proxy(url))
+        if student_url == None:
+            print("Must specify --student-agent Agent,name,websocket")
+            sys.exit(1)
+        print("Connecting to {}".format(url))
+        asyncio.get_event_loop().run_until_complete(proxy(student_url,StudentAgent, studentAgent_name))
     else:
         snake=SnakeGame(hor=60, ver=40, fps=20, visual=visual, obstacles=15, mapa=inputfile)
         snake.setPlayers([  
-            StudentAgent([snake.playerPos()], name=StudentAgent_name),
-            OponentAgent([snake.playerPos()], name=OponentAgent_name),
+            StudentAgent([snake.playerPos()], name=studentAgent_name) if student_url == None else StudentAgent([snake.playerPos()], name=student_name, url=student_url),
+            OponentAgent([snake.playerPos()], name=oponentAgent_name) if oponent_url == None else OponentAgent([snake.playerPos()], name=oponentAgent_name, url=oponent_url),
         ])
         snake.start()
 
-async def proxy(url):
+async def proxy(url, StudentAgent, agent_name):
     async with websockets.connect(url) as websocket:
         logger = logging.getLogger('websockets')
         logger.setLevel(logging.ERROR)
@@ -62,7 +79,7 @@ async def proxy(url):
         #connect to proxy, get init values and announce ourselves through the agent name
         await websocket.send(json.dumps({'cmd': 'PROXY'}))
         init = json.loads(await websocket.recv())
-        agent = StudentAgent([(b[0], b[1]) for b in init['body']],(init['direction'][0], init['direction'][1]), name = OponentAgent_name)
+        agent = StudentAgent([(b[0], b[1]) for b in init['body']],(init['direction'][0], init['direction'][1]), name = agent_name)
         await websocket.send(agent.name)
 
         while True:
