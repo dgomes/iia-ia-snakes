fa6da884288a291405b2d9049c1c9d6d7f8f82aa
Diogo Gomes <dgomes@av.it.pt>
2016-11-16 00:31:31 +0000

    proxy <-> agent naming

--- netserver.py
+++ netserver.py
@@ -3,35 +3,50 @@ import asyncio
 import websockets
 import json
 import sys
+import logging
 
-agent = None
+logging.basicConfig(format=':%(levelname)s:%(message)s', level=logging.INFO)
 proxy = None
+agent = None
 
-async def proxyserver(websocket, path):
-    global agent, proxy
+async def agentserver(websocket, path):
+    global proxy, agent
 
-    _msg = await websocket.recv()
-    msg = json.loads(_msg) 
-    print("INIT: {}".format(msg))
-    if msg['cmd'] == "PROXY":
-        proxy = websocket
-        while True:
-            m = await proxy.recv()
-            print("PROXY: {}".format(m))
-            await agent.send(m)
-        
-    else:
-        agent = websocket
-        await proxy.send(_msg)
-        while True:
-            m = await agent.recv()
-            print("AGENT: {}".format(m))
-            await proxy.send(m)
+    try:
+        _msg = await websocket.recv()
+        msg = json.loads(_msg) 
+        logging.info("INIT: {}".format(msg))
+        if msg['cmd'] == "HELLO":
+            agent = websocket
+            while True:
+                m = await agent.recv()
+                logging.debug("AGENT: {}".format(m))
+                await proxy.send(m)
+            
+        else:
+            proxy = websocket
+            if agent == None:
+                logging.error("Agent must connect before Proxy") 
+                proxy.close()
+                return
+            await agent.send(_msg) #proxy 1st message is the __init__ that must be sent to the agent
+            while True:
+                m = await proxy.recv()
+                logging.debug("PROXY: {}".format(m))
+                await agent.send(m)
+
+    except websockets.exceptions.ConnectionClosed as e:
+        if proxy != None:
+            proxy.close(1001,"Other end closed")
+            proxy = None
+        if agent != None:
+            agent.close(1001,"Other end closed")
+            agent = None
 
 if len(sys.argv) < 2:
     print("Usage: python3 {} port_number".format(sys.argv[0]))
     sys.exit(1)
-start_server = websockets.serve(proxyserver, port=int(sys.argv[1]))
+start_server = websockets.serve(agentserver, port=int(sys.argv[1]))
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
