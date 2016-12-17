aca8fd200742406174aadce7adc486c2b075d7f1
Diogo Gomes <dgomes@av.it.pt>
2016-12-17 22:38:51 +0000

    add gameid

--- netagent.py
+++ netagent.py
@@ -7,9 +7,9 @@ import logging
 import sys
 
 class NetAgent(Snake):
-    def __init__(self,body=[(0,0)] , direction=(1,0), name="network", url="ws://localhost:8765"):
+    def __init__(self,body=[(0,0)] , direction=(1,0), name="network", url="ws://localhost:8765", gameid=None):
         self.ws = create_connection(url)
-        self.ws.send(json.dumps({'cmd': 'PROXY', 'agent_name': name}))
+        self.ws.send(json.dumps({'cmd': 'PROXY', 'agent_name': name, 'gameid': str(gameid)}))
         super().__init__(body,direction,name=name)
         self.ws.send(json.dumps({'cmd':'init', 'body':body, 'direction':direction}))
         self.name = self.ws.recv()
